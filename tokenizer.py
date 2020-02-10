5b226ea117e4302ebe178500daccb76a00013901
ptaejoon <gomataejoon@gmail.com>
2020-02-10 15:32:11 +0900

    khaii error fixed

--- tokenizer.py
+++ tokenizer.py
@@ -1,8 +1,8 @@
 from khaiii import KhaiiiApi
 
+khaii = KhaiiiApi()
 def tokenize(sentence):
 	token = []
-	khaii = KhaiiiApi()
 	for word in khaii.analyze(sentence):
 		for morph in word.morphs:
 			token.append(morph.lex)
