40e64a492ec1660d1df971774daed8bcc58457c8
ptaejoon <gomataejoon@gmail.com>
2020-02-11 14:44:45 +0900

    GAN 저장 혹은 새로 만들어서 저장해두는 파일. 이거 이용해서 실험해야 빠르게 추출가능

---
+++ GAN_result_tester.py
@@ -0,0 +1,24 @@
+import pickle
+import stockGAN
+import os
+
+
+ganFileName = "stockGAN.sav"
+if os.path.isfile(ganFileName):
+    print("Saved GAN Model Already Exists. Loading stockGAN.sav")
+    file = open(ganFileName, 'rb')
+    gan = pickle.load(file)
+else:
+    print("Saved GAN Model Doesn't Exists. Saving stockGAN.sav")
+    file = open(ganFileName,'wb')
+    sav_gan = stockGAN.GAN()
+    sav_gan.train()
+    pickle.dump(sav_gan,file)
+    gan = sav_gan
+
+predicted_2019 = gan.predict_testSet()
+print(predicted_2019)
+
+
+
+
